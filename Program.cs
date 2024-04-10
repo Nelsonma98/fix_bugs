be78832ce0783027e9477b9a04d4f64cfde4be70
SpNel <mendozaalvareznelson@gmail.com>
2024-04-09 22:48:15 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace fix_bugs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution([-1, 1, -2, 2]));
+    }
+
+    static int Solution(int[] numbers)
+    {
+        int small = 0;
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < small)
+            {
+                small = numbers[i];
+            }
+        }
+        return small;
+    }
+}
